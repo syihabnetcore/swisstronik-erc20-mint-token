c5425a1cf7fb2282c801d5c99eb64050bdb7a71f
syihabnetcore <119837774+syihabnetcore@users.noreply.github.com>
2024-07-24 18:37:53 +0700

    Create Token.sol

---
+++ contracts/Token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("LangkyToken","LNKY"){} 
+
+    function mint1000000tokens() public {
+        _mint(msg.sender,1000000*10**18);
+    }
+
+    function burn1000tokens() public{
+        _burn(msg.sender,1000000*10**18);
+    }
+    
+}
